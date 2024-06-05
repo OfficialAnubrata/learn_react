6e3e0c1e93857cfc203e24a99d502768fca31183
Anubrata Sarkar <anubrata.sarkar@iemlabs.com>
2024-06-05 13:23:18 +0530

    custom react work in vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,8 +3,14 @@ import Chai from "./chai"
 
 
 function App() {
+  const username = "Anubrata Sarkar"
   return (
+    <>
     <Chai/>
+    <h1>chai  to {username}</h1> 
+    {/* evaluted Expression */}
+    </>
+
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+// const reactElement = {
+//   type: 'a',
+//   props : {
+//       href: 'https://www.google.com',
+//       target: '_blank',
+//   },
+//   children : 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href='https://www.google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "Sonai"
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://www.google.com', target: '_blank'},
+  'Click me to visit Google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // MyApp()
+  reactElement
+  // anotherElement
 )
