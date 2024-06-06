ec138cff5c424acf3fe7be3b26cae55a4b0f82c9
Anubrata Sarkar <anubrata.sarkar@iemlabs.com>
2024-06-06 12:59:51 +0530

    add and remove counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let  [Counter, setCounter] = useState(0)
+
+  // let Counter = 5
+
+  const addValue = () => {
+    console.log("clicked", Counter);
+    // Counter = Counter + 1
+    if(Counter < 20){
+      setCounter(Counter + 1)
+    }
+    
+  }
+  const removeValue = () => {
+    console.log("clicked", Counter);
+    // Counter = Counter - 1
+    if(Counter > 0){
+      setCounter(Counter - 1)
+    }
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {Counter}</h2>
+
+      <button 
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Decrease Value</button>
+    </>
+  )
+}
+
+export default App
